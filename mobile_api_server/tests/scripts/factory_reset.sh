0aa79b0ed3e4513e59d30692512ef815c6f24385
Ossi Saukko <ossi.saukko@centria.fi>
2022-12-13 13:59:48 +0200

    Small Changes to Make CI Work with D-Bus
    These changes should make CI testing work using the D-Bus for checking
    that command scripts are run.
    
    There is a known race condition issue with factory reset and
    configuration endpoints. The problem is that both of these tests share
    the same configuration file and are run parallel.
    
    The integration test was intended to fix race condition problems, but it
    started to turn out complex and unstable. Therefore, that path was
    deemed unworthy. Instead, we plan to modify mobile_api to use paths
    given to it instead of using the environment variables. The server
    should still use the environment variable and tell it for the library.
    This way, unit tests can run on separate folders, thus fixing race
    condition problems.

--- mobile_api_server/tests/scripts/factory_reset.sh
+++ mobile_api_server/tests/scripts/factory_reset.sh
@@ -2,4 +2,5 @@
 
 # Unit test is checking that script was run using D-Bus
 SCRIPT="${0##*/}"
-busctl --user call eu.sifis_home.Testing.FactoryReset /Testing eu.sifis_home.Testing ScriptWasRun s "${SCRIPT}"
+dbus-send --type=method_call --dest=eu.sifis_home.Testing.FactoryReset /Testing eu.sifis_home.Testing.ScriptWasRun "string:${SCRIPT}"
+echo "${SCRIPT} was run"
